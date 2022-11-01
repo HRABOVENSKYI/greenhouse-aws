32675f953048166573f779359a57eed8664079ac
Teodor Hrabovenskyi <teodor.hrabovenskyi@dataart.com>
2022-11-01 15:26:40 +0200

    Initial commit

---
+++ greenhouse-api/README.md
@@ -0,0 +1,6 @@
+# GreenHouse
+Swagger URL -> http://localhost:8088/swagger-ui/index.html
+
+Actuator URL -> http://localhost:8088/sensors/manage
+
+For actuator we overrided default URL in the application.properties
---
+++ load-service/src/test/java/ua/lviv/dataart/loadservice/LoadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.dataart.loadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
